3a376de99c43eb6cbdd9892e9d9f93d238b63218
caujolle herve <herve.caujolle@gmail.com>
2023-10-30 16:25:15 +0100

    28. Mapping de base d'une entité

--- src/main/java/com/hibernate4all/tutorial/domain/Movie.java
+++ src/main/java/com/hibernate4all/tutorial/domain/Movie.java
@@ -1,27 +1,35 @@
 package com.hibernate4all.tutorial.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name = "MOVIE")
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    // On passe les annotations sur les attributs, les fields en Anglais.
+    // On appelle cela du mapping en field access.
+    @Column(nullable = false) // contrainte not null en base de données
     private String name;
 
+    // mapping explicite : on nomme explicitement les noms des colonnes
+    // grâce à @Column.
+    @Column(name = "description")
+    private String description;
     public String getDescription() {
         return description;
     }
 
+    // Indique que l'attibut ne doit pas être mappé avec la BD.
+    //@Transient
     public void setDescription(String description) {
         this.description = description;
     }
-
-    private String description;
 
     public Long getId() {
         return id;
